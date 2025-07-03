2686d9945a90cbc5d5788249223c677325f449aa
tushargithub52 <tusharraiku6020@gmail.com>
2025-07-04 00:00:20 +0530

    implemented create notes in mini notes app

--- Day-02/server.js
+++ Day-02/server.js
@@ -3,6 +3,22 @@ const express = require("express")
 //server ko create krte hai 
 const app = express()
 
+//middleware to read the req.body data in json format
+app.use(express.json());
+
+//initialize the notes array
+notes=[]
+
+app.post('/notes', (req, res) => {
+    console.log(req.body);
+    notes.push(req.body);
+    res.json({
+        message: "Notes added successfully",
+        notes: notes,
+    })
+})
+
+//server ko start krte hai 
 app.listen(3000, () => {
     console.log("Server is running on port 3000")
 })
